5ce63c3e40fe13655196c1bcbea078e3cd2a80c9
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2015-03-30 23:29:06 +0300

    setTheoreticDifference function template added

--- utils/utils.h
+++ utils/utils.h
@@ -4,6 +4,7 @@
 #include <functional>
 #include <set>
 #include <algorithm>
+#include <iterator>
 
 // Qt utils
 
@@ -39,6 +40,19 @@ inline Container uniqueElements(const Container& c)
 	Container result;
 	detail::NotDuplicatePredicate<typename Container::value_type> predicate;
 	std::copy_if(c.begin(), c.end(), std::back_inserter(result), std::ref(predicate));
+	return result;
+}
+
+template <template<typename...> class OutputContainerType, class ContainerType1, class ContainerType2>
+OutputContainerType<typename ContainerType1::value_type, std::allocator<typename ContainerType1::value_type>> setTheoreticDifference(ContainerType1 c1, ContainerType2 c2,
+	const std::function<bool(const typename ContainerType1::value_type&, const typename ContainerType1::value_type&)>& comp = [](const typename ContainerType1::value_type& l, const typename ContainerType1::value_type& r) {return l < r;})
+{
+	OutputContainerType<typename ContainerType1::value_type, std::allocator<typename ContainerType1::value_type>> result;
+	std::sort(c1.begin(), c1.end(), comp);
+	std::sort(c2.begin(), c2.end(), comp);
+
+	std::set_difference(c1.begin(), c1.end(), c2.begin(), c2.end(), std::back_inserter(result), comp);
+
 	return result;
 }
 
