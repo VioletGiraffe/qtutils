9400854e8b5a85ae9661952330321aa29d770c27
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2015-03-30 21:53:22 +0300

    utils.h: some Qt and STL helper functions

--- utils/utils.h
+++ utils/utils.h
@@ -1,4 +1,44 @@
-#ifndef UTILS_H
-#define UTILS_H
+#pragma once
 
-#endif // UTILS_H
+#include "../QtIncludes"
+#include <functional>
+#include <set>
+#include <algorithm>
+
+// Qt utils
+
+inline void qtimerSingleShot(const std::function<void()>& lambda, int delay)
+{
+	QTimer * timer = new QTimer;
+	timer->setSingleShot(true);
+	QObject::connect(timer, &QTimer::timeout, [=]() {
+		(lambda)();
+		timer->deleteLater();
+	});
+	timer->start(delay);
+}
+
+// STL utils
+
+namespace detail {
+	template <typename T>
+	struct NotDuplicatePredicate
+	{
+		bool operator()(const T& element)
+		{
+			return _s.insert(element).second; // true if a new element was inserted
+		}
+	private:
+		std::set<T> _s;
+	};
+}
+
+template <class Container>
+inline Container uniqueElements(const Container& c)
+{
+	Container result;
+	detail::NotDuplicatePredicate<typename Container::value_type> predicate;
+	std::copy_if(c.begin(), c.end(), std::back_inserter(result), std::ref(predicate));
+	return result;
+}
+
