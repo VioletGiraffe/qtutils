515aa8a3a8f1a677bf81468d2f5a26910872270c
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2015-04-01 20:13:34 +0300

    CSettings refactoring - mutex and global QSettings object removed

--- settings/csettings.cpp
+++ settings/csettings.cpp
@@ -1,40 +1,20 @@
 #include "csettings.h"
 
-std::shared_ptr<QSettings> CSettings::_settings;
 QString CSettings::_applicationName;
 QString CSettings::_organizationName;
 
-#ifndef NO_THREADSAFE_CSETTINGS
-QMutex CSettings::_settingsMutex;
-#endif
+
+std::unique_ptr<QSettings> CSettings::instance()
+{
+	return std::move(std::unique_ptr<QSettings>(new QSettings(QSettings::NativeFormat, QSettings::UserScope,
+		_organizationName.isEmpty() ? qApp->organizationName() : _organizationName,
+		_applicationName.isEmpty() ? qApp->applicationName() : _applicationName)));
+}
+
+
 
 CSettings::CSettings()
 {
-#ifndef NO_THREADSAFE_CSETTINGS
-	QMutexLocker locker (&_settingsMutex);
-#endif
-	if (_settings.get() == 0)
-	{
-		_settings = std::make_shared<QSettings>(QSettings::NativeFormat, QSettings::UserScope,
-												_organizationName.isEmpty() ? qApp->organizationName() : _organizationName,
-												_applicationName.isEmpty() ? qApp->applicationName() : _applicationName);
-	}
-}
-
-void CSettings::setValue(const QString &key, const QVariant &value)
-{
-#ifndef NO_THREADSAFE_CSETTINGS
-	QMutexLocker locker (&_settingsMutex);
-#endif
-	_settings->setValue( key, value );
-}
-
-QVariant CSettings::value(const QString &key, const QVariant &defaultValue) const
-{
-#ifndef NO_THREADSAFE_CSETTINGS
-	QMutexLocker locker (&_settingsMutex);
-#endif
-	return _settings->value( key, defaultValue );
 }
 
 void CSettings::setApplicationName(const QString &name)
