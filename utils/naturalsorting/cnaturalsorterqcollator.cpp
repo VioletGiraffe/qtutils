7e473500701a8f5e1cd6c72475015285f6977238
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2015-04-30 00:04:28 +0300

    Unused parameter commented out

--- utils/naturalsorting/cnaturalsorterqcollator.cpp
+++ utils/naturalsorting/cnaturalsorterqcollator.cpp
@@ -6,7 +6,7 @@ CNaturalSorterQCollator::CNaturalSorterQCollator()
 	_collator.setNumericMode(true);
 }
 
-bool CNaturalSorterQCollator::compare(const QString & l, const QString & r, SortingOptions options) const
+bool CNaturalSorterQCollator::compare(const QString & l, const QString & r, SortingOptions /*options*/) const
 {
 	return _collator.compare(l, r) == -1;
 }
