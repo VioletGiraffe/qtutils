06ce128ed658c7cf1e10268a580c96c4b0efaad9
VioletGiraffe <alexxxx89@ya.ru>
2018-10-15 23:30:18 +0300

    qdebug_helpers.hpp: operator<< for std::array

--- qtcore_helpers/qdebug_helpers.hpp
+++ qtcore_helpers/qdebug_helpers.hpp
@@ -3,6 +3,7 @@
 #include "../std_helpers/qt_string_helpers.hpp"
 
 DISABLE_COMPILER_WARNINGS
+#include <QByteArray>
 #include <QDebug>
 RESTORE_COMPILER_WARNINGS
 
@@ -14,4 +15,16 @@ inline QDebug& operator<<(QDebug& debug, const std::string& std_str)
 inline QDebug operator<<(QDebug&& debug, const std::string& std_str)
 {
 	return debug << qStrFromStdStrU8(std_str);
+}
+
+template <typename T, size_t N>
+QDebug operator<<(QDebug&& debug, const std::array<T, N>& ar)
+{
+	return debug << QByteArray::fromRawData(ar.data(), ar.size()).toHex();
+}
+
+template <typename T, size_t N>
+QDebug& operator<<(QDebug& debug, const std::array<T, N>& ar)
+{
+	return debug << QByteArray::fromRawData((const char*)ar.data(), (int)ar.size() * sizeof(T)).toHex();
 }
