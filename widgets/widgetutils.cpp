9a10ff4640fd61974affbe941c3d7afba4514157
VioletGiraffe <noreply@github.com>
2022-04-22 00:39:09 +0300

    More WidgetUtils

--- widgets/widgetutils.cpp
+++ widgets/widgetutils.cpp
@@ -1,10 +1,11 @@
 #include "widgetutils.h"
-#include "compiler/compiler_warnings_control.h"
 
 DISABLE_COMPILER_WARNINGS
 #include <QApplication>
+#include <QDesktopWidget>
 #include <QLayout>
 #include <QMainWindow>
+#include <QScreen>
 #include <QWidget>
 RESTORE_COMPILER_WARNINGS
 
@@ -60,3 +61,61 @@ bool WidgetUtils::widgetBelongsToHierarchy(QWidget* const widget, QObject* const
 
 		return false;
 }
+
+QRect WidgetUtils::currentScreenGeometryForWidget(QWidget *widget)
+{
+	const int screenIndex = QApplication::desktop()->screenNumber(widget);
+	if (const auto* primaryScreen = QApplication::primaryScreen(); screenIndex < 0)
+		return primaryScreen ? primaryScreen->availableGeometry() : QRect{};
+
+	const auto screens = QApplication::screens();
+	if (screenIndex >= screens.size())
+	{
+		Q_ASSERT(screenIndex < screens.size());
+		const auto* primaryScreen = QApplication::primaryScreen();
+		return primaryScreen ? primaryScreen->availableGeometry() : QRect{};
+	}
+
+	return screens[screenIndex]->availableGeometry();
+}
+
+QRect WidgetUtils::geometryAtCenter(const QRect &reference, qreal scale)
+{
+	return geometryAtCenter(reference, reference.size() * scale);
+}
+
+QRect WidgetUtils::geometryAtCenter(const QRect &reference, const QSize &size)
+{
+	QRect centeredRect(QPoint{0, 0}, size);
+	centeredRect.moveCenter(reference.center());
+	return centeredRect;
+}
+
+void WidgetUtils::centerWidgetInRect(QWidget *widget, const QRect &rect, const QSize &newWidgetSize)
+{
+	widget->setGeometry(geometryAtCenter(rect, newWidgetSize.isValid() ? newWidgetSize : widget->size()));
+}
+
+void WidgetUtils::centerWidgetOnScreen(QWidget *widget, const QSize &newWidgetSize)
+{
+	centerWidgetInRect(widget, currentScreenGeometryForWidget(widget), newWidgetSize);
+}
+
+void WidgetUtils::centerWidgetInParent(QWidget *widget, const QSize &newWidgetSize)
+{
+	if (widget->parentWidget())
+		centerWidgetInRect(widget, widget->parentWidget()->geometry(), newWidgetSize);
+	else
+		centerWidgetOnScreen(widget, newWidgetSize);
+}
+
+void WidgetUtils::centerWidgetOnScreen(QWidget *widget, qreal fractionOfScreenSize)
+{
+	centerWidgetOnScreen(widget, currentScreenGeometryForWidget(widget).size() * fractionOfScreenSize);
+}
+
+void WidgetUtils::centerWidgetInParent(QWidget *widget, qreal fractionOfParentSize)
+{
+	centerWidgetInParent(widget, widget->parentWidget() ? (widget->parentWidget()->size() * fractionOfParentSize) : widget->size());
+}
+
--- widgets/widgetutils.h
+++ widgets/widgetutils.h
@@ -1,8 +1,14 @@
 #pragma once
+#include "compiler/compiler_warnings_control.h"
+
+DISABLE_COMPILER_WARNINGS
+#include <QSize>
+RESTORE_COMPILER_WARNINGS
 
 class QLayout;
 class QMainWindow;
 class QObject;
+class QRect;
 class QWidget;
 
 namespace WidgetUtils
@@ -11,4 +17,16 @@ namespace WidgetUtils
 	QMainWindow* findParentMainWindow(QWidget* child);
 	QMainWindow* findTopLevelWindow();
 	bool widgetBelongsToHierarchy(QWidget * const widget, QObject * const hierarchy);
+
+	QRect currentScreenGeometryForWidget(QWidget* widget);
+	QRect geometryAtCenter(const QRect& reference, qreal scale);
+	QRect geometryAtCenter(const QRect& reference, const QSize& size);
+
+	void centerWidgetInRect(QWidget* widget, const QRect& rect, const QSize& newWidgetSize = QSize(-1, -1));
+
+	void centerWidgetOnScreen(QWidget* widget, const QSize& newWidgetSize = QSize(-1, -1));
+	void centerWidgetOnScreen(QWidget* widget, qreal fractionOfScreenSize);
+
+	void centerWidgetInParent(QWidget* widget, const QSize& newWidgetSize = QSize(-1, -1));
+	void centerWidgetInParent(QWidget* widget, qreal fractionOfParentSize);
 }
