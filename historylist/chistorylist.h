20fdec32a41ccec81c7c7c66863198806a0f30da
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2023-03-06 11:15:38 +0200

    Code formatting

--- historylist/chistorylist.h
+++ historylist/chistorylist.h
@@ -11,27 +11,29 @@ class CHistoryList
 {
 public:
 	using container_type = std::vector<T>;
-	CHistoryList() : _currentIndex{ size_t_max }
+	explicit CHistoryList(size_t maxSize = 2000) noexcept :
+		_currentIndex{ size_t_max },
+		_maxSize{ maxSize }
 	{}
 
-// Access and status
-	inline bool empty() const {return _list.empty();}
-	inline size_t size() const {return _list.size();}
-	inline void clear() {_list.clear();}
+	// Access and status
+	inline bool empty() const { return _list.empty(); }
+	inline size_t size() const { return _list.size(); }
+	inline void clear() { _list.clear(); }
 
-	inline typename container_type::const_iterator begin() const {return _list.begin();}
-	inline typename container_type::const_iterator end() const {return _list.end();}
-	inline typename container_type::const_reverse_iterator rbegin() const {return _list.rbegin();}
-	inline typename container_type::const_reverse_iterator rend() const {return _list.rend();}
+	inline typename container_type::const_iterator begin() const { return _list.begin(); }
+	inline typename container_type::const_iterator end() const { return _list.end(); }
+	inline typename container_type::const_reverse_iterator rbegin() const { return _list.rbegin(); }
+	inline typename container_type::const_reverse_iterator rend() const { return _list.rend(); }
 
-	inline const T& front() const {return _list.front();}
-	inline const T& back() const {return _list.back();}
+	inline const T& front() const { return _list.front(); }
+	inline const T& back() const { return _list.back(); }
 
-	inline const T& operator[](size_t index) const {return _list[index];}
+	inline const T& operator[](size_t index) const { return _list[index]; }
 
-	inline bool historyLocationSet() const {return _currentIndex <= _list.size();}
+	inline bool historyLocationSet() const { return _currentIndex <= _list.size(); }
 
-	inline const container_type& list() const {return _list;}
+	inline const container_type& list() const { return _list; }
 
 	size_t currentIndex() const;
 	const T& currentItem() const;
@@ -39,7 +41,7 @@ public:
 	bool isAtEnd() const;
 	bool isAtBeginning() const;
 
-// Actions
+	// Actions
 	void addLatest(const T& item);
 	void addLatest(const std::vector<T>& items);
 	const T& navigateBack();
@@ -48,12 +50,13 @@ public:
 private:
 	container_type _list;
 	size_t         _currentIndex;
+	const size_t   _maxSize;
 };
 
 template <typename T>
 const T& CHistoryList<T>::navigateForward()
 {
-	if (_currentIndex < size()-1)
+	if (_currentIndex < size() - 1)
 		_currentIndex = currentIndex() + 1;
 	return currentItem();
 }
@@ -82,7 +85,7 @@ template <typename T>
 bool CHistoryList<T>::isAtEnd() const
 {
 	assert_r(empty() || _currentIndex < size());
-	return !empty() && _currentIndex == size()-1;
+	return !empty() && _currentIndex == size() - 1;
 }
 
 template <typename T>
@@ -104,7 +107,7 @@ size_t CHistoryList<T>::currentIndex() const
 }
 
 template <typename T>
-void CHistoryList<T>::addLatest( const T& item )
+void CHistoryList<T>::addLatest(const T& item)
 {
 	if (currentItem() == item)
 		return;
@@ -116,7 +119,7 @@ void CHistoryList<T>::addLatest( const T& item )
 		std::rotate(currentPosition, currentPosition + 1, _list.end());
 
 	assert_r(!empty());
-	if (size() > 1 && _currentIndex <= size()-2) // If we already were at the end of the list, just move the index to the newest element
+	if (size() > 1 && _currentIndex <= size() - 2) // If we already were at the end of the list, just move the index to the newest element
 	{
 		// If we were not in the end, move and insert [0; _currentIndex] just before end
 		decltype(_list) newList;
@@ -128,12 +131,12 @@ void CHistoryList<T>::addLatest( const T& item )
 		_list.emplace_back(item);
 	}
 
-	_currentIndex = _list.size()-1;
+	_currentIndex = _list.size() - 1;
 }
 
 template <typename T>
-void CHistoryList<T>::addLatest(const std::vector<T> & items)
-{	
+void CHistoryList<T>::addLatest(const std::vector<T>& items)
+{
 	_list = SetOperations::uniqueElements<SetOperations::ItemOrder::KeepLastOccurrence>(items);
 	_currentIndex = _list.size() - 1;
 }
