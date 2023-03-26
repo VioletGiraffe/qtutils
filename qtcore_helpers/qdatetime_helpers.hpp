769302181485fa90fe5409c337bdc4e939f2384c
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2023-03-26 17:56:47 +0300

    Warnings

--- qtcore_helpers/qdatetime_helpers.hpp
+++ qtcore_helpers/qdatetime_helpers.hpp
@@ -1,6 +1,9 @@
 #pragma once
+#include "compiler/compiler_warnings_control.h"
 
+DISABLE_COMPILER_WARNINGS
 #include <QDateTime>
+RESTORE_COMPILER_WARNINGS
 
 [[nodiscard]] inline time_t toTime_t(const QDateTime& date) noexcept
 {
