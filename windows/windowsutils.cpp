72ce7421c637cc315d45776afd1a5d729d04e1fc
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2024-09-11 23:12:02 +0300

    Bug in toUncWcharArray() fixed

--- windows/windowsutils.cpp
+++ windows/windowsutils.cpp
@@ -14,31 +14,22 @@ inline wchar_t* append(wchar_t* dst, const wchar_t (&src)[N])
 	return dst + N - 1; // Null was already part of the input literal
 }
 
-QString toUncPath(QString somePath)
-{
-	somePath.replace('/', '\\');
-
-	if (somePath.contains(':')) // Quick and dirty test for absolute path
-		return somePath.prepend(R"(\\?\)");
-	else
-		return somePath;
-}
-
-void detail::toUncWcharArray(const QString& somePath, wchar_t* array, size_t arraySize)
+size_t detail::toUncWcharArray(const QString& somePath, wchar_t* array, size_t arraySize)
 {
 	wchar_t* currentStrStart = array;
 	if (somePath.contains(':')) // Quick and dirty test for absolute path
 		currentStrStart = append(array, LR"(\\?\)");
 
 	const size_t len = (size_t)somePath.toWCharArray(currentStrStart);
-	assert_debug_only(len < arraySize - (array - currentStrStart));
+	const auto prefixLength = currentStrStart - array;
+	assert_debug_only(len < arraySize - prefixLength);
 
-	// Replace backslashes with forward slashes
 	for (size_t i = 0; i < len; ++i)
 	{
-		if (currentStrStart[i] == L'\\')
-			currentStrStart[i] = L'/';
+		if (currentStrStart[i] == L'/')
+			currentStrStart[i] = L'\\';
 	}
 
 	currentStrStart[len] = L'\0';
+	return prefixLength + len + 1 /* null terminator */;
 }
--- windows/windowsutils.h
+++ windows/windowsutils.h
@@ -4,13 +4,11 @@ class QString;
 
 namespace detail {
 
-void toUncWcharArray(const QString& path, wchar_t* array, size_t arraySize);
+size_t toUncWcharArray(const QString& path, wchar_t* array, size_t arraySize);
 
 } // namespace detail
 
-QString toUncPath(QString somePath);
-
 template <size_t N>
-void toUncWcharArray(const QString& path, wchar_t(&array)[N]) {
-	detail::toUncWcharArray(path, array, N);
+size_t toUncWcharArray(const QString& path, wchar_t(&array)[N]) {
+	return detail::toUncWcharArray(path, array, N);
 }
