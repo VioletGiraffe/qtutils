97893a6af36645520da9b377c4e4f8911d509254
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2026-01-20 16:38:47 +0200

    CPersistenceEnabler: support for immediate (non-delayed) state restoration

--- widgets/cpersistentwindow.cpp
+++ widgets/cpersistentwindow.cpp
@@ -13,32 +13,30 @@ RESTORE_COMPILER_WARNINGS
 #define GEOMETRY_KEY (_settingsPath + "_geometry")
 #define STATE_KEY (_settingsPath + "_state")
 
-CPersistenceEnabler::CPersistenceEnabler(QString widgetSettingsPath, QObject* parent) :
+CPersistenceEnabler::CPersistenceEnabler(QString widgetSettingsPath, QObject* parent, bool delayed) :
 	QObject(parent),
-	_settingsPath{std::move(widgetSettingsPath)}
+	_settingsPath{std::move(widgetSettingsPath)},
+	_delayed{ delayed }
 {
+	if (!_delayed)
+	{
+		QWidget* widget = qobject_cast<QWidget*>(parent);
+		assert_and_return_r(widget, );
+		restoreState(widget);
+	}
 }
 
 bool CPersistenceEnabler::eventFilter(QObject* watched, QEvent* e)
 {
-	if (!_windowStateRestored && e->type() == QEvent::Show)
+	if (_delayed && !_windowStateRestored && e->type() == QEvent::Show) [[unlikely]]
 	{
 		_windowStateRestored = true;
 
 		auto* widget = static_cast<QWidget*>(watched);
 		assert_and_return_r(widget, QObject::eventFilter(watched, e));
-		auto* window = dynamic_cast<QMainWindow*>(watched);
-
-		CSettings s;
-
-		if (!widget->restoreGeometry(s.value(GEOMETRY_KEY).toByteArray()) || !window || !window->restoreState(s.value(STATE_KEY).toByteArray()))
-		{
-			const auto * const currentScreen = QApplication::screenAt(widget->geometry().center());
-			const auto availableGeometry = currentScreen ? currentScreen->availableGeometry() : QApplication::primaryScreen()->availableGeometry();
-			widget->setGeometry(QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter, availableGeometry.size() / 2, availableGeometry));
-		}
+		restoreState(widget);
 	}
-	else if (e->type() == QEvent::Close)
+	else if (e->type() == QEvent::Close) [[unlikely]]
 	{
 		auto* widget = static_cast<QWidget*>(watched);
 		auto* window = dynamic_cast<QMainWindow*>(watched);
@@ -50,4 +48,17 @@ bool CPersistenceEnabler::eventFilter(QObject* watched, QEvent* e)
 	}
 
 	return QObject::eventFilter(watched, e);
+}
+
+void CPersistenceEnabler::restoreState(QWidget* widget)
+{
+	auto* window = dynamic_cast<QMainWindow*>(widget);
+	CSettings s;
+
+	if (!widget->restoreGeometry(s.value(GEOMETRY_KEY).toByteArray()) || !window || !window->restoreState(s.value(STATE_KEY).toByteArray()))
+	{
+		const auto* const currentScreen = QApplication::screenAt(widget->geometry().center());
+		const auto availableGeometry = currentScreen ? currentScreen->availableGeometry() : QApplication::primaryScreen()->availableGeometry();
+		widget->setGeometry(QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter, availableGeometry.size() / 2, availableGeometry));
+	}
 }
--- widgets/cpersistentwindow.h
+++ widgets/cpersistentwindow.h
@@ -6,16 +6,22 @@ DISABLE_COMPILER_WARNINGS
 #include <QObject>
 RESTORE_COMPILER_WARNINGS
 
+class QWidget;
+
 class CPersistenceEnabler final : public QObject
 {
 public:
 	// Set widgetSettingsPath to an application-wide-unique QSettings path+name for storing this widget's state and position between application launches
-	CPersistenceEnabler(QString widgetSettingsPath, QObject* parent = nullptr);
+	explicit CPersistenceEnabler(QString widgetSettingsPath, QObject* parent = nullptr, bool delayed = true);
 
 protected:
 	bool eventFilter(QObject *watched, QEvent *event) override;
 
 private:
+	void restoreState(QWidget* widget);
+
+private:
 	const QString _settingsPath;
 	bool _windowStateRestored = false;
+	const bool _delayed;
 };
