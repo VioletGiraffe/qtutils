441f1ac9ff600ec764a4279cf42114cf61bd096a
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2026-07-15 13:35:51 +0300

    NaturalSort: case sensitivity fixed

--- utils/naturalsorting/cnaturalsorterqcollator.h
+++ utils/naturalsorting/cnaturalsorterqcollator.h
@@ -7,22 +7,30 @@ DISABLE_COMPILER_WARNINGS
 RESTORE_COMPILER_WARNINGS
 
 namespace NaturalSort {
-	inline bool lessThan(const QString& l, const QString& r, bool caseSensitive = true) noexcept {
-		thread_local static QCollator collator = [caseSensitive]() {
+	[[nodiscard]] inline bool lessThan(const QString& l, const QString& r, bool caseSensitive = true) noexcept {
+		thread_local static QCollator collatorCaseSensitive = []() {
 			QCollator c;
-			c.setCaseSensitivity(caseSensitive ? Qt::CaseSensitive : Qt::CaseInsensitive);
+			c.setCaseSensitivity(Qt::CaseSensitive);
+			c.setNumericMode(true);
+			return c;
+		}();
+
+		thread_local static QCollator collatorCaseInsensitive = []() {
+			QCollator c;
+			c.setCaseSensitivity(Qt::CaseInsensitive);
 			c.setNumericMode(true);
 			return c;
 		}();
 
 		// Fix for the new breaking changes in QCollator in Qt 5.14 - null strings are no longer a valid input
-		return collator.compare(qToStringViewIgnoringNull(l), qToStringViewIgnoringNull(r)) < 0;
+		return (caseSensitive ? collatorCaseSensitive : collatorCaseInsensitive).compare(qToStringViewIgnoringNull(l), qToStringViewIgnoringNull(r)) < 0;
 	}
 
-	inline bool lessCaseSensitive(const QString& l, const QString& r) noexcept {
+	[[nodiscard]] inline bool lessCaseSensitive(const QString& l, const QString& r) noexcept {
 		return lessThan(l, r, true);
 	}
-	inline bool lessCaseInsensitive(const QString& l, const QString& r) noexcept {
+
+	[[nodiscard]] inline bool lessCaseInsensitive(const QString& l, const QString& r) noexcept {
 		return lessThan(l, r, false);
 	}
 
