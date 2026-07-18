a83fc52e8ad9d84480e19638217c25b2d0e25e9b
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2026-07-19 02:50:38 +0300

    CLoggerInMemory: thread safety

--- logger/cloggerinmemory.h
+++ logger/cloggerinmemory.h
@@ -2,6 +2,9 @@
 
 #include "cloggerinterface.h"
 
+#include <mutex>
+
+// Thread-safe: intended as a global sink fed from an installed Qt message handler, which fires on any thread.
 class CLoggerInMemory : public CLoggerInterface
 {
 public:
@@ -9,5 +12,6 @@ public:
 	[[nodiscard]] QStringList contents() const override;
 
 private:
+	mutable std::mutex _mutex;
 	QStringList _entries;
 };
