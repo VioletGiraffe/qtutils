a7b5cd59bcdb3bc4107fcbe597c41fb25c2c4abf
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2026-09-05 20:07:33 +0300

    private namespaces

--- windows/windowsutils.h
+++ windows/windowsutils.h
@@ -2,13 +2,13 @@
 
 class QString;
 
-namespace detail {
+namespace windows_utils_detail {
 
 size_t toUncWcharArray(const QString& path, wchar_t* array, size_t arraySize);
 
-} // namespace detail
+} // namespace windows_utils_detail
 
 template <size_t N>
 size_t toUncWcharArray(const QString& path, wchar_t(&array)[N]) {
-	return detail::toUncWcharArray(path, array, N);
+	return windows_utils_detail::toUncWcharArray(path, array, N);
 }
